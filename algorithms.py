de973160ceb0282bb9e9a803069545fcf0dafeb2
monstucture <monstucture@gmail.com>
2020-07-13 11:04:26 -0500

    Update

--- algorithms.py
+++ algorithms.py
@@ -74,4 +74,36 @@ class MergeSort(Algorithm):
             if left[i] < right[i]:
                 result.append(left[i])
                 i+=1
-            pass
+            else:
+                result.append(right[i])
+                j += 1
+            self.update_dis()
+        result += left[i:]
+        result += right[j:]
+        self.array = result
+        self.update_dis()
+        return result
+
+class QuickSort(Algorithm):
+    def __init__(self):
+        super().__init__("QuickSort")
+
+    def algorithm(self, array=[], start=0, end=0):
+        if array == []:
+            array = self.array
+            end = len(array) - 1
+        if start < end:
+            pivot = self.partition(array,start,end)
+            self.algorithm(array,start,pivot-1)
+            self.algorithm(array,pivot+1,end)
+
+    def partition(self, array, start, end):
+        x = array[end]
+        i = start-1
+        for j in range(start, end+1, 1):
+            if array[j] <= x:
+                i += 1
+                if i < j:
+                    array[i], array[j] = array[j], array[i]
+                    self.update_display(array[i], array[j])
+        return i
